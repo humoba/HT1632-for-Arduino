a69a167c8b07a247cbaadc0965409595ec08c06d
Hugo <hugo@unocero.com>
2016-05-03 18:22:05 -0500

    setCLK ddded
    Without "setCLK", can't send information to the display. In this example, the clock is in the pin number 8.

--- Arduino/HT1632/examples/HT1632_Text_8X4_Multidisplay/HT1632_Text_8X4_Multidisplay.ino
+++ Arduino/HT1632/examples/HT1632_Text_8X4_Multidisplay/HT1632_Text_8X4_Multidisplay.ino
@@ -7,6 +7,7 @@ int wd;
 char disp [] = "Hello, how are you?";
 
 void setup () {
+  HT1632.setCLK(8);
   HT1632.begin(12, 13, 10, 9);
   
   wd = HT1632.getTextWidth(disp, FONT_8X4_END, FONT_8X4_HEIGHT);
